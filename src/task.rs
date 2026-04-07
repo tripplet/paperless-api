41e932f4d8320e11a46815b26a8492b5b5c87d94
Tobias Tangemann <tobias@tangemann.org>
2026-04-07 11:38:03 +0200

    Use proper enums for task fields

--- src/task.rs
+++ src/task.rs
@@ -9,14 +9,24 @@ pub struct TaskId(pub String);
 /// A paperless task
 #[derive(Debug, Clone, Deserialize)]
 pub struct Task {
-    pub id: i32,
+    pub id: u32,
+
+    /// The Celery-ID of the task.
     pub task_id: TaskId,
 
+    /// The name/king of the task.
     #[serde(rename = "task_name")]
-    pub name: String,
+    pub name: TaskName,
 
-    pub owner: i32,
-    pub status: String,
+    /// The type of the task.
+    #[serde(rename = "type")]
+    pub task_type: TaskType,
+
+    /// The status of the task.
+    pub status: TaskStatus,
+
+    pub owner: UserId,
+
     pub acknowledged: bool,
     pub result: Option<String>,
     pub related_document: Option<String>,
@@ -26,4 +36,36 @@ impl std::fmt::Display for TaskId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
+}
+
+/// The status of a task.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum TaskStatus {
+    Failure,
+    Pending,
+    Received,
+    Retry,
+    Revoked,
+    Started,
+    Success,
+}
+
+/// The name of a task.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum TaskName {
+    ConsumeFile,
+    TrainClassifier,
+    CheckSanity,
+    IndexOptimize,
+}
+
+/// The type of a task.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum TaskType {
+    AutoTask,
+    ScheduledTask,
+    ManualTask,
 }
